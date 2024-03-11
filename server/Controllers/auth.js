a0b3fe6cdd798bb2f5002958b581cf05a1d907e2
shivam <shivammahrolia@gmail.com>
2024-03-11 14:14:39 +0530

    deployment

---
+++ server/Controllers/auth.js
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/auth.js'
+
+export const login = async(req,res) => {
+    const {email} = req.body;
+    try {
+        const existingUser = await User.findOne({email})
+        if(!existingUser){
+            try {
+                const newUser = await  User.create({email});
+
+                const token = jwt.sign({
+                    email:newUser.email, id:newUser._id 
+                }, process.env.JWT_SECRET,{
+                    expiresIn:"1h"
+                } )
+                console.log("try",token)
+
+                res.status(200).json({result:newUser, token})
+
+            } catch (error) {
+                res.status(500).json({message:"Something went wrong in looginn",error})
+            }
+        } else {
+            console.log("enter")
+            const token = jwt.sign({
+                email:existingUser.email, id:existingUser._id 
+            }, process.env.JWT_SECRET,{
+                expiresIn:"1h"
+            } )
+
+            res.status(200).json({result:existingUser, token})  
+        }
+
+    } catch (error) {
+        res.status(500).json({message:"Something went wrong in laogin",error})
+    }
+}
