c0cc919f9d20866df4e1539f1c5e99de380cb8ac
shivam <122466461+Honey-kr-12@users.noreply.github.com>
2024-05-12 00:02:24 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
 import express from "express"
 import dontenv from "dotenv"
 import connectionToDatabase from "./db/connectionToDatabase.js"
-import userRoutes from './Routes/user.js'
+// import userRoutes from './Routes/user.js'
 import cors from 'cors'
 import bodyParser from "body-parser"
-import videoRoutes from './routes/video.js'
-import commentsRoutes from './routes/comments.js'
-import stripePackage from  'stripe'
-import paymentRoutes from './Routes/payment.js'
-import path from 'path'
-import requestIp from 'request-ip'
+// import videoRoutes from './routes/video.js'
+// import commentsRoutes from './routes/comments.js'
+// import stripePackage from  'stripe'
+// import paymentRoutes from './Routes/payment.js'
+// import path from 'path'
+// import requestIp from 'request-ip'
 
-const quantity = 25;
+// const quantity = 25;
 
 
 // const __variableOfChoice = path.resolve();
@@ -20,8 +20,8 @@ dontenv.config();
 
 const app=express()
 app.use(cors());
-app.use(express.json({limit:"30mb", extended:true}));
-app.use(express.urlencoded({limit:"30mb", extended:true}));
+// app.use(express.json({limit:"30mb", extended:true}));
+// app.use(express.urlencoded({limit:"30mb", extended:true}));
 // app.use('/uploads',express.static(path.join('uploads')))
 
 
@@ -31,11 +31,11 @@ app.get('/',(req,res) => {
 
 app.use(bodyParser.json());
 app.set('trust proxy', true);
-app.use(requestIp.mw());
-app.use('/user',userRoutes);
-app.use('/video',videoRoutes);
-app.use('/comment',commentsRoutes);
-app.use('/premium/api/subscriptions',paymentRoutes);
+// app.use(requestIp.mw());
+// app.use('/user',userRoutes);
+// app.use('/video',videoRoutes);
+// app.use('/comment',commentsRoutes);
+// app.use('/premium/api/subscriptions',paymentRoutes);
 // app.use('/api/create-subscription-checkout-session',async);
 
 
