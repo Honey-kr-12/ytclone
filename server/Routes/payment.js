dfa78bb7a1fd754bf6d0f2ab64bd20dbbdcb6d06
shivam <122466461+Honey-kr-12@users.noreply.github.com>
2024-05-12 00:11:29 +0530

    Delete server/Routes/payment.js

--- server/Routes/payment.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {paymentController} from  '../Controllers/payment.js'
-
-const routes = express.Router()
-
-routes.post('/',paymentController);
-
-export default routes;  
